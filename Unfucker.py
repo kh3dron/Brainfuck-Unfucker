0abd40525dbb494a75c304234aead20b5c31cc8d
Tristan Saldanha <tristansaldanha@gmail.com>
2018-08-18 22:01:27 -0500

    Program logic done!
    Now just to polish and add proper docs

--- Unfucker.py
+++ Unfucker.py
@@ -1,49 +1,58 @@
 readme = """
 UNFUCKER: A BRAINFUCK EXECUTOR IN PYTHON
 by KHEDRON
+
+Use for any and all purposes!
 """
-logging = True
-#Upon becoming more familiar with the language, I see now that this
-#probably spells out a message of sorts
-numberList = """
-+>++>+++>>>+++++--->++
-"""
+#I'm going to assume this is better left on by default.
+logging = False
 
-#A single loop
-fourTimesFivePlusTwo = """
-++++[>+++++<-]>++
-"""
+#HAD TO BE FED the string the the [ at the BEGINNING.
+def jumpUp(text):
+#    print "Jumping up on", text,
+    skips = 0
+    clock = 0
+    for n in text:
+        #print clock, n
+        clock += 1
+        if n == "[":
+            skips += 1
+        elif n == "]":
+            skips -= 1
+            if skips == 0:
+                break
+        #print "skips at", skips
+    #print "found clock of ", clock
+    return clock -1
+#HAS TO BE FED the string with the ] at the END.
+def jumpBack(text):
+    #print "Jumping back on", text,
+    skips = 0
+    clock = 0
+    for n in reversed(text):
+        clock += 1
+        #print clock, n
+        if n == "[":
+            skips -= 1
+            if skips == 0:
+                break
+        elif n == "]":
+            skips += 1
+        #print "skips at", skips
+    clock -= 2
+    #print "found clock of", clock
+    return clock +1
 
-#Another single loop program
-HelloWorld = """
-++++++++++[>+++++++>++++++++++>+++>+<<<<-]
->++.>+.+++++++..+++.>++.<<+++++++++++++++.
->.+++.------.--------.>+.>.
-"""
-
-#multiple levels of loops, current challenge
-fib = """
-+++++[->----[---->+<]>++.-[++++>---<]>.++.---------.+++.[++>---<]>--.++[->+++<]>.+++++++++..---.+++++++.+[-->+++++<]>-.<]
-"""
-
-
-def locateEndLoop(text):
-    n = len(text)-1
-    while n > 0:
-        if text[n] == "]":
-            found = n
-            break
-        n -= 1
-    return text[:found]
-
-def recursableCompiling(file, cells, pointer):
-    print "Currently compiling\n", file
-
+def Compile(file):
     legals = ["+", "-", "[", "]", ">", "<"]
-
+    cells = [0]
+    pointer = 0
     t = 0
+
     while t < len(file):
         n = file[t]
+        t+= 1
+
 
         if n == "+":
             cells[pointer] +=1
@@ -58,20 +67,41 @@ def recursableCompiling(file, cells, pointer):
         elif n == ".":
             print cells[pointer]
         elif n == ",":
-            print "INPUT VALUE FOR CELL", pointer, ":",
+            print "Input value for cell", pointer, ":",
             cells[pointer] = int(raw_input())
 
         elif n == "[":
-            isolatedLoop = locateEndLoop(file[t+1:])
-            while cells[pointer] > 1:
-                cells = recursableCompiling(isolatedLoop, cells, pointer)
+            if cells[pointer] == 0:
+                t += jumpUp(file[t:])
+        elif n == "]":
+            if cells[pointer] != 0:
+                t -= jumpBack(file[:t])
 
-        if n in legals:
-            if logging:
-                print n, "tick", cells #console for debugging
-            else:
-                fish = "glub glub"
-        t+= 1
+        if (n in legals) & (logging):
+            print "Instruction", t, "is", n, ", pointer index:", pointer, cells
+
     return cells
 
-print recursableCompiling(fib, [0], 0)
+#untested...
+def clean(text):
+    scrubbed = ""
+    legals = ["+", "-", "[", "]", ">", "<"]
+    for n in text:
+        if n in legals:
+            scrubbed += n
+    return n
+
+#This bit of logic borrowed from a similar project
+def main():
+  if len(sys.argv) == 2:
+      Compile(sys.argv[1])
+  else:
+      print("Usage:", sys.argv[0], "filename")
+      #ya dingus
+
+file = '''+>+<[[>>>+<<<-]>[>+>+<<-]>.]'''
+
+print Compile(file)
+
+#fact: once you use one of these in a python program you've made it
+#if __name__ == "__main__": main()
